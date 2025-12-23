486d86be25b78c62b51f013eddfb04be63308957
itsamanlotey <aman@sanshtech.ca>
2025-12-23 10:31:13 -0700

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -66,6 +67,8 @@ export default function RootLayout({
         <Header />
         <main className="w-full">{children}</main>
         <Footer />
+         {/* ✅ Vercel Analytics */}
+        <Analytics />
       </body>
     </html>
   );
